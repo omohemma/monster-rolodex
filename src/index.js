f7527f733c246d43c6bfdea18b773df4758ab227
Omohemma <emmaomoh@gmail.com>
2021-08-16 08:46:29 +0100

    Update GitIgnore & Comment on index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // to write JSx in javascript file
+import ReactDOM from 'react-dom'; // to interact DOM to effect the state changes in our app
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
