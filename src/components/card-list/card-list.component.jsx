a77865c07e0f0bd3bb11c8a071979b65b1f075ba
Omohemma <emmaomoh@gmail.com>
2021-08-17 11:27:55 +0100

    Create CardComponent and receive monster props from CardList Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,12 @@
 import './card-list.styles.css'
+import {CardComponent} from "../card/card.component";
 
 // Houses The cards which is received by props - like slot in vue.js
+// Receives props from Parent and passes to Card Component
 export const CardList = (props) => {
   return  <div className="card-list">
     {
-      props.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+      props.monsters.map(monster => <CardComponent key={monster.id} monster={monster}/>)
     }
   </div>
 }
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import './card.styles.css'
+
+export const CardComponent = (props) => {
+  return <div className='card-container'>
+    <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt={props.monster.name}/>
+    <h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+  </div>
+}
