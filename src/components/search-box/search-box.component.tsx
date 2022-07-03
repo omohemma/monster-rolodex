4b95480b5bb8066e584ae190ed84769a10fe0ec3
Omohemma <emmaomoh@gmail.com>
2022-07-04 00:20:29 +0100

    Starting our Typing

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,4 +1,7 @@
 import './search-box.styles.css'
+
+const name : string =   "Omololu Emmanuel"
+
 export const  SearchBoxComponent = ({placeholder, HandleChange}) => {
   return <input className='search' type="search" placeholder={placeholder} onChange={HandleChange}/>
 }
