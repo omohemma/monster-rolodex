639298ba0fa0073cbbbc667dd757229d7440b256
Omohemma <emmaomoh@gmail.com>
2022-07-04 00:30:57 +0100

    Typescript Interfaces

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,7 +1,16 @@
 import './search-box.styles.css'
 
-const name : string =   "Omololu Emmanuel"
+/**
+ * Interface can be extended and overloaded
+ */
+interface ISearchBoxProps {
+  placeholder?: string;
+}
 
-export const  SearchBoxComponent = ({placeholder, HandleChange}) => {
+interface ISearchBoxProps {
+  HandleChange: (event: Event) => void;
+}
+
+export const  SearchBoxComponent = ({placeholder, HandleChange} : ISearchBoxProps) => {
   return <input className='search' type="search" placeholder={placeholder} onChange={HandleChange}/>
 }
