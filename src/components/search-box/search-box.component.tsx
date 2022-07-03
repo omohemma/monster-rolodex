d123246fb5c27ee4f5e591fddaa8a1d948b6f5a9
Omohemma <emmaomoh@gmail.com>
2022-07-04 00:40:47 +0100

    Typescript Types & Third Party Library Types

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -2,6 +2,7 @@ import './search-box.styles.css'
 
 /**
  * Interface can be extended and overloaded
+ * Types afford us a feature called union
  */
 interface ISearchBoxProps {
   placeholder?: string;
@@ -11,6 +12,29 @@ interface ISearchBoxProps {
   HandleChange: (event: Event) => void;
 }
 
-export const  SearchBoxComponent = ({placeholder, HandleChange} : ISearchBoxProps) => {
+type CanadianAddress = {
+  state: string;
+  street: string;
+}
+
+type USAddress = {
+  province: string;
+  street: string
+}
+
+type NorthAmericanAddress = CanadianAddress | USAddress
+
+const address : NorthAmericanAddress = {
+  street: "Allen Avenue",
+  state: "Oklahoma",
+}
+
+
+type SearchBoxProps = {
+  placeholder?: string;
+  HandleChange: (event: Event) => void;
+}
+
+export const  SearchBoxComponent = ({placeholder, HandleChange} : SearchBoxProps) => {
   return <input className='search' type="search" placeholder={placeholder} onChange={HandleChange}/>
 }
